93a2bf99614738b7bf226f8d4001646e71aa052b
Andre Simao <111400782+andresima0@users.noreply.github.com>
2023-09-28 02:32:08 -0300

    Update MacAddressTextWatcher.java

--- mobileLed_Bt_v4/MacAddressTextWatcher.java
+++ mobileLed_Bt_v4/MacAddressTextWatcher.java
@@ -2,13 +2,20 @@ package br.com.local.mobileled_bt_v4;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.EditText;
 
-class MacAddressTextWatcher implements TextWatcher {
+public class MacAddressTextWatcher implements TextWatcher {
 
-    private static final int MAC_ADDRESS_BLOCK_SIZE = 2;
-    private static final char MAC_ADDRESS_SEPARATOR = ':';
+    private static final int MAC_ADDRESS_LENGTH = 12; // Total length of the MAC address with mask
+    private static final int MAC_ADDRESS_BLOCK_SIZE = 2; // Size of each block in the MAC address
+    private static final char MAC_ADDRESS_SEPARATOR = ':'; // MAC address separator
+    private boolean isFormatting; // Control variable to avoid infinite loops
 
-    private boolean isFormatting;
+    private final EditText editText;
+
+    public MacAddressTextWatcher(EditText editText) {
+        this.editText = editText;
+    }
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -20,15 +27,26 @@ class MacAddressTextWatcher implements TextWatcher {
 
     @Override
     public void afterTextChanged(Editable s) {
+        // Check if we are already formatting the text to avoid infinite loops
         if (isFormatting) {
             return;
         }
 
+        // Mark that we are formatting the text
         isFormatting = true;
 
-        String originalText = s.toString();
-        String formattedText = originalText.replaceAll(":", "");
+        // Remove all current separators (colons)
+        String originalText = s.toString().replaceAll(":", "");
 
+        // Convert letters to uppercase
+        String formattedText = originalText.toUpperCase();
+
+        // Limit the length to 12 characters (without separators)
+        if (formattedText.length() > MAC_ADDRESS_LENGTH) {
+            formattedText = formattedText.substring(0, MAC_ADDRESS_LENGTH);
+        }
+
+        // Reinsert separators every two characters
         StringBuilder finalText = new StringBuilder();
         for (int i = 0; i < formattedText.length(); i++) {
             finalText.append(formattedText.charAt(i));
@@ -37,8 +55,13 @@ class MacAddressTextWatcher implements TextWatcher {
             }
         }
 
-        s.replace(0, s.length(), finalText);
+        // Set the formatted text back into the input field
+        editText.removeTextChangedListener(this);
+        editText.setText(finalText);
+        editText.setSelection(finalText.length());
+        editText.addTextChangedListener(this);
 
+        // Restore the control variable
         isFormatting = false;
     }
 }
